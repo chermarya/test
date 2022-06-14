140376b6a639d5586e6c20e4523aabf6145fc98a
chermarya <106978580+chermarya@users.noreply.github.com>
2022-06-15 01:21:15 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,49 @@
+function ukr () {
+    document.getElementById("text_main").innerHTML = "Головна";
+    document.getElementById("text_contact").innerHTML = "Контакти";
+    document.getElementById("name").innerHTML = "Аліна Цилєнко";
+    document.getElementById("profession").innerHTML = "сімейний та кризовий психолог";
+    document.getElementById("sphere").innerHTML = "Працюю з широким спектром запитів, зокрема:";
+    document.getElementById("res1").innerHTML = "Міжособистісні відносини";
+    document.getElementById("res2").innerHTML = "Відносини з дітьми";
+    document.getElementById("res3").innerHTML = "Особисті межі";
+    document.getElementById("res4").innerHTML = "Питання, пов'язані із самооцінкою";
+    document.getElementById("res5").innerHTML = "Самореалізація та самодостатність";
+    document.getElementById("res6").innerHTML = "Співзалежні відносини, токсичні відносини, вихід із них";
+    document.getElementById("res7").innerHTML = "Переживання стресових та кризових періодів, у тому числі пов'язаних з військовими діями";
+    document.getElementById("formName").placeholder = "Ім'я";
+    document.getElementById("formMessage").placeholder = "Повідомлення";
+    document.getElementById("send_btn").innerHTML = "Відправити";
+    document.getElementById("form__title").innerHTML = "Записатись на консультацію";
+    document.getElementById("").innerHTML = "";
+    document.getElementById("").innerHTML = "";
+    document.getElementById("").innerHTML = "";
+    document.getElementById("").innerHTML = "";
+    document.getElementById("").innerHTML = "";
+    document.getElementById("").innerHTML = "";
+}
+
+function rus () {
+    document.getElementById("text_main").innerHTML = "Главная";
+    document.getElementById("text_contact").innerHTML = "Контакты";
+    document.getElementById("name").innerHTML = "Алина Циленко";
+    document.getElementById("profession").innerHTML = "семейный и кризисный психолог";
+    document.getElementById("sphere").innerHTML = "Работаю  с широким спектром запросов, в частности:";
+    document.getElementById("res1").innerHTML = "Mежличностные отношения";
+    document.getElementById("res2").innerHTML = "Отношения с детьми";
+    document.getElementById("res3").innerHTML = "Личные границы";
+    document.getElementById("res4").innerHTML = "Вопросы, связанные с самооценкой";
+    document.getElementById("res5").innerHTML = "Самореализация и  самодостаточность";
+    document.getElementById("res6").innerHTML = "Созависимые отношения, токсичные отношения, выход из них";
+    document.getElementById("res7").innerHTML = "Переживания стрессовых и кризисных периодов, в том числе и связанных с военными действиями";
+    document.getElementById("formName").placeholder = "Имя";
+    document.getElementById("formMessage").placeholder = "Сообщение";
+    document.getElementById("send_btn").innerHTML = "Отправить";
+    document.getElementById("form__title").innerHTML = "Записаться на консультацию";
+    document.getElementById("").innerHTML = "";
+    document.getElementById("").innerHTML = "";
+    document.getElementById("").innerHTML = "";
+    document.getElementById("").innerHTML = "";
+    document.getElementById("").innerHTML = "";
+    document.getElementById("").innerHTML = "";
+}
